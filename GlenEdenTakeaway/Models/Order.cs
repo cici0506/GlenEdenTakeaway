494dd46611d2cc22775e0fce739d3351e7bb994e
Cindy Kuang <cindykuang811@gmail.com>
2023-06-28 13:15:18 +1200

    added models and inserted table names

---
+++ GlenEdenTakeaway/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿namespace GlenEdenTakeaway.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string Quantity { get; set; }
+        public string ItemName { get; set; }
+        public decimal Price {get; set;}
+        public int CustomerId { get; set; }
+        public int EmployeeId { get; set; }
+        public virtual ICollection<Customer> Customer { get; set; } = new List<Customer>();
+
+        public virtual ICollection<Employee> Employee { get; set; } = new List<Employee>();
+        public virtual ICollection<Payment> Payment { get; set; } = new List<Payment>();
+    }
+}
